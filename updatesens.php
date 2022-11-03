8182369fbe8558064ad908d2606ef769666c9a02
dhomanhuri <dhomanhuri@gmail.com>
2022-11-03 20:13:07 +0700

    sens

--- updatesens.php
+++ updatesens.php
@@ -1,16 +1,14 @@
 <?php
-$mysqli = new mysqli("promaydo.net","promaydo_ersa","ersa_polinema","promaydo_ersa");
+
 $hum = $_GET['hum'];
 $temp = $_GET['temp'];
 $telur = $_GET['telur'];
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-$query = "UPDATE `datasensor` SET `temp` = '$temp', `hum` = '$hum',`telur` = '$telur' WHERE `datasensor`.`id` = 1;";
+$conn = mysqli_connect("promaydo.net","promaydo_ersa","ersa_polinema","promaydo_ersa");
 
-mysqli_query($mysqli, $query);
-echo "success";
+$data = mysqli_query($conn,"UPDATE `datasensor` SET `temp` = '$temp',`hum` = '$hum', `telur` = '$telur' WHERE `datasensor`.`id` = 1;");
+
+echo "ss";
+
+
 ?>
