4afa1dfb81faa10293d664a0ca3a2f64943efc69
dhomanhuri <dhomanhuri@gmail.com>
2022-11-03 20:31:54 +0700

    ass

--- updatesens.php
+++ updatesens.php
@@ -5,7 +5,10 @@ $temp = $_GET['temp'];
 $telur = $_GET['telur'];
 
 $conn = mysqli_connect("promaydo.net","promaydo_ersa","ersa_polinema","promaydo_ersa");
-
+if ($conn -> connect_error) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+  }
 $data = mysqli_query($conn,"UPDATE `datasensor` SET `temp` = '$temp',`hum` = '$hum', `telur` = '$telur' WHERE `datasensor`.`id` = 1;");
 
 echo "ss";
